be8d02d4457edb83dbe8f8e4e653c6ae5cfc99e6
Utkarsh1504 <umishra1504@gmail.com>
2024-03-29 23:47:44 +0530

    cors fixes in deployment

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger";
 import pricingRoutes from "./routes/pricingRoutes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
